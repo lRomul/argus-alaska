31cb7fa2064e621c3472cac1fc6b639e7a2d6c93
ruslan <ruslan1123@gmail.com>
2020-05-10 22:36:07 +0300

    feat: Add transforms

---
+++ src/transforms.py
@@ -0,0 +1,69 @@
+import torch
+import random
+import numpy as np
+
+
+class Compose:
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image, trg=None):
+        if trg is None:
+            for t in self.transforms:
+                image = t(image)
+            return image
+        else:
+            for t in self.transforms:
+                image, trg = t(image, trg)
+            return image, trg
+
+
+class UseWithProb:
+    def __init__(self, transform, prob=.5):
+        self.transform = transform
+        self.prob = prob
+
+    def __call__(self, image, trg=None):
+        if trg is None:
+            if random.random() < self.prob:
+                image = self.transform(image)
+            return image
+        else:
+            if random.random() < self.prob:
+                image, trg = self.transform(image, trg)
+            return image, trg
+
+
+class OneOf:
+    def __init__(self, transforms, p=None):
+        self.transforms = transforms
+        self.p = p
+
+    def __call__(self, image, trg=None):
+        transform = np.random.choice(self.transforms, p=self.p)
+        if trg is None:
+            image = transform(image)
+            return image
+        else:
+            image, trg = transform(image, trg)
+            return image, trg
+
+
+class ImageToTensor:
+    def __call__(self, image):
+        image = np.moveaxis(image, -1, 0)
+        image = image.astype(np.float32) / 255.0
+        image = torch.from_numpy(image)
+        return image
+
+
+def get_transforms(train):
+    if train:
+        transforms = Compose([
+            ImageToTensor()
+        ])
+    else:
+        transforms = Compose([
+            ImageToTensor()
+        ])
+    return transforms
