f6cf23a79d8e1055ea84c11fc9592c5120c2b592
ruslan <ruslan1123@gmail.com>
2020-05-10 18:58:21 +0300

    feat: Add AlaskaDataset

--- src/datasets.py
+++ src/datasets.py
@@ -1,4 +1,6 @@
 import cv2
+import time
+import random
 import numpy as np
 import pandas as pd
 from src import config
@@ -42,3 +44,58 @@ def get_folds_data():
         folds_data.append(sample)
 
     return folds_data
+
+
+class AlaskaDataset(Dataset):
+    def __init__(self,
+                 data,
+                 folds=None,
+                 target=True,
+                 transform=None,
+                 mixer=None):
+        self.folds = folds
+        self.target = target
+        self.transform = transform
+        self.mixer = mixer
+
+        self.data = data
+
+        if folds is not None:
+            self.data = [s for s in self.data if s['fold'] in folds]
+
+    def __len__(self):
+        return len(self.data)
+
+    def get_sample(self, idx):
+        random_class = np.random.choice(config.classes)
+        sample = self.data[idx][random_class]
+
+        image = decode_raw_image(sample['raw_image'])
+
+        if not self.target:
+            return image
+
+        target = torch.tensor(sample['target'], dtype=torch.int64)
+        return image, target
+
+    def _set_random_seed(self, idx):
+        seed = int(time.time() * 1000.0) + idx
+        random.seed(seed)
+        np.random.seed(seed % (2**32 - 1))
+
+    @torch.no_grad()
+    def __getitem__(self, idx):
+        self._set_random_seed(idx)
+
+        if not self.target:
+            image = self.get_sample(idx)
+            if self.transform is not None:
+                image = self.transform(image)
+            return image
+        else:
+            image, target = self.get_sample(idx)
+            if self.mixer is not None:
+                image, target = self.mixer(self, image, target)
+            if self.transform is not None:
+                image = self.transform(image)
+            return image, target
