f3261af689e14b1a339d3b67804465f857cf42ff
ruslan <ruslan1123@gmail.com>
2020-06-27 15:19:51 +0300

    feat: Leave swish activation

--- src/models/timm_model.py
+++ src/models/timm_model.py
@@ -5,35 +5,22 @@ from timm import create_model
 from src.models.classifiers import Classifier
 
 
-def get_timm_activations():
-    import inspect
-    import timm
-
-    members = inspect.getmembers(timm.models.layers.activations, inspect.isclass)
-    return tuple([m[1] for m in members])
-
-
-def convert_layers(model, layer_type_old, layer_type_new,
-                   convert_weights=False, p_name='', p_module=None):
-    activations = get_timm_activations()
+def convert_layers(model, layer_type_old,
+                   layer_type_new, convert_weights=False):
     for name, module in reversed(model._modules.items()):
         if len(list(module.children())) > 0:
-            model._modules[name] = convert_layers(module, layer_type_old, layer_type_new,
-                                                  convert_weights, p_name, p_module)
+            model._modules[name] = convert_layers(module, layer_type_old,
+                                                  layer_type_new, convert_weights)
 
-        if type(module) == layer_type_old and isinstance(p_module, activations):
-            model._modules[p_name] = nn.Identity()
+        if type(module) == layer_type_old:
             layer_old = module
-            layer_new = (layer_type_new(module.num_features)
-                         if hasattr(module, 'num_features') else layer_type_new())
+            layer_new = layer_type_new(module.num_features, activation="identity")
 
             if convert_weights:
                 layer_new.weight = layer_old.weight
                 layer_new.bias = layer_old.bias
 
             model._modules[name] = layer_new
-        p_name, p_module = name, module
-
     return model
 
 
