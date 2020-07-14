3067ca168a1702eab583711f15bb266dbf43bdda
ruslan <ruslan1123@gmail.com>
2020-07-14 12:39:03 +0300

    feat: Add tta and logit flags to files names

--- predict.py
+++ predict.py
@@ -17,13 +17,18 @@ EXPERIMENT_DIR = config.experiments_dir / args.experiment
 PREDICTION_DIR = config.predictions_dir / args.experiment
 DEVICE = 'cuda'
 BATCH_SIZE = 32
-IMAGE_SIZE = None
-LOGITS = True
+LOGITS = False
+TTA = True
+
+NAME = args.experiment
+if TTA:
+    NAME += '-tta'
+if LOGITS:
+    NAME += '-logits'
 
 
-def predict_test_fold(test_data, predictor, fold):
-    fold_prediction_dir = PREDICTION_DIR / f'fold_{fold}' / 'test'
-    fold_prediction_dir.mkdir(parents=True, exist_ok=True)
+def predict_test_fold(test_data, predictor):
+    PREDICTION_DIR.mkdir(parents=True, exist_ok=True)
 
     image_names = [s['name'] for s in test_data]
 
@@ -31,7 +36,7 @@ def predict_test_fold(test_data, predictor, fold):
     altered_pred, quality_pred = preds
 
     np.savez(
-        fold_prediction_dir / 'preds.npz',
+        PREDICTION_DIR / f'preds-{NAME}.npz',
         altered_pred=altered_pred,
         quality_pred=quality_pred,
         name=image_names,
@@ -40,32 +45,11 @@ def predict_test_fold(test_data, predictor, fold):
     preds_df = pd.DataFrame(index=image_names, columns=config.classes)
     preds_df.index.name = 'Id'
     preds_df.values[:] = altered_pred
-    preds_df.to_csv(fold_prediction_dir / f'{PREDICTION_DIR.name}.csv')
+    preds_df.to_csv(PREDICTION_DIR / f'pred-{NAME}.csv')
 
-
-def blend_folds_submission():
-    image_names = None
-    altered_pred_lst = []
-    for fold in config.folds:
-        fold_prediction_path = PREDICTION_DIR / f'fold_{fold}' / 'test' / 'preds.npz'
-        if not fold_prediction_path.exists():
-            continue
-        preds = np.load(fold_prediction_path)
-        altered_pred = preds['altered_pred']
-        altered_pred = target2altered(altered_pred)
-        altered_pred_lst.append(altered_pred)
-
-        names = preds['name']
-        if image_names is not None:
-            assert np.all(image_names == names)
-        image_names = names
-
-    altered_pred = np.stack(altered_pred_lst, axis=0)
-    altered_pred = np.mean(altered_pred, axis=0)
-
-    pred_df = pd.DataFrame({'Id': image_names,
-                            'Label': altered_pred})
-    pred_df.to_csv(PREDICTION_DIR / 'submission.csv', index=False)
+    subm_df = pd.DataFrame({'Id': image_names,
+                            'Label': target2altered(altered_pred)})
+    subm_df.to_csv(PREDICTION_DIR / f'subm-{NAME}.csv', index=False)
 
 
 if __name__ == "__main__":
@@ -73,24 +57,16 @@ if __name__ == "__main__":
         transforms = get_transforms(train=False)
         test_data = get_test_data()
 
-        for fold in config.folds:
-            print("Predict fold", fold)
-            fold_dir = EXPERIMENT_DIR / f'fold_{fold}'
-            model_path = get_best_model_path(fold_dir)
+        fold_dir = EXPERIMENT_DIR / f'fold_0'
+        model_path = get_best_model_path(fold_dir)
 
-            if model_path is None:
-                print("Skip fold", fold)
-                continue
+        print("Model path", model_path)
+        predictor = Predictor(model_path,
+                              batch_size=BATCH_SIZE,
+                              transform=transforms,
+                              device=DEVICE,
+                              logits=LOGITS,
+                              tta=TTA)
 
-            print("Model path", model_path)
-            predictor = Predictor(model_path,
-                                  batch_size=BATCH_SIZE,
-                                  transform=transforms,
-                                  device=DEVICE,
-                                  logits=LOGITS)
-
-            print("Test predict")
-            predict_test_fold(test_data, predictor, fold)
-
-        print("Blend folds predictions")
-        blend_folds_submission()
+        print("Test predict")
+        predict_test_fold(test_data, predictor)
